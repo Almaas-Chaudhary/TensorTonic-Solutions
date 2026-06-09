cc904a6d2439a85d00fd0bb0eaacdbd4ca148d2e
Almaas-Chaudhary <183535988+Almaas-Chaudhary@users.noreply.github.com>
2026-06-09 08:37:32 +0530

    Solve transformers-attention

--- transformer/transformers-attention/transformers-attention.py
+++ transformer/transformers-attention/transformers-attention.py
@@ -6,6 +6,7 @@ def scaled_dot_product_attention(Q: torch.Tensor, K: torch.Tensor, V: torch.Tens
     """
     Compute scaled dot-product attention.
     """
+    # because the k, v and q are dimension batch, seq_len, d_k so we want only last and second last part of the k,v and q
     scores = torch.matmul(Q, K.transpose(-2, -1)) / math.sqrt(K.shape[-1])
 
     weights = F.softmax(scores, dim=-1)
